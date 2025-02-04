5a8cdc4d2bc0a0d45dafa1f727311702db0624b9
Rafael Pototsky Lourenço <rafaelpototskylourenco10@gmail.com>
2025-02-04 17:38:00 -0300

    add CourseComponent and integrate it into the app module; update header styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { TrailAboutMeComponent } from './components/trail-about-me/trail-about-me.component';
+import { CourseComponent } from './components/course/course.component';
 
 @NgModule({
   declarations: [	
@@ -13,6 +14,7 @@ import { TrailAboutMeComponent } from './components/trail-about-me/trail-about-m
     HeaderComponent,
     NavigationComponent,
     TrailAboutMeComponent,
+    CourseComponent,
   ],
   imports: [
     BrowserModule,
