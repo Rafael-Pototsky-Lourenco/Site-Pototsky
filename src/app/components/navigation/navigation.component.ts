29ae9276596fd8dfee5cf06caae8b822db605a9c
Rafael P Lourenço <rafael.lourenco@consilux.com.br>
2025-02-05 16:54:50 -0300

    refactor TrailAboutMe component to use dynamic data binding; simplify navigation component structure

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -8,16 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
-  aboutMe1: string = 'Entre dezembro de 2022 á junho de 2023, eu trabalhei como recepcionista no Hotel Caiobá, onde desempenhei funções de atendimento ao público, check-in e check-out de hóspedes, forneci informações e suporte para garantir uma estadia agradável aos clientes.';
-  work1: string = 'Hotel Caiobá';
-  year1: number = 2022;
-
-  aboutMe2: string = 'Entre julho de 2023 á dezembro de 2023, eu trabalhei no setor financeiro do Super Rede Atacadista, após receber uma promoção. Minhas responsabilidades incluíam o fechamento de caixa dos mercados, garantindo a precisão e organização dos registros financeiros.';
-  work2: string = 'Super Rede Atacadista';
-  year2: number = 2023;
-
-  aboutMe3: string = 'Desde janeiro de 2024, após mudar de cidade, eu trabalho na área da computação na empresa Consilux Consultoria. Durante esse período, aprendi a lidar com C# e SQL Server e, atualmente, atuo como desenvolvedor FrontEnd, programando em Angular.';
-  work3: string = 'Consilux Consultoria';
-  year3: number = 2024;
-  
+  abouts = [
+    {
+      aboutMe: 'Entre dezembro de 2022 á junho de 2023, eu trabalhei como recepcionista no Hotel Caiobá, onde desempenhei funções de atendimento ao público, check-in e check-out de hóspedes, forneci informações e suporte para garantir uma estadia agradável aos clientes.',
+      work: 'Hotel Caiobá',
+      year: '2022',
+      image: '../../../assets/imagens/dayOne.jpg',
+    },
+    {
+      aboutMe: 'Entre julho de 2023 á dezembro de 2023, eu trabalhei no setor financeiro do Super Rede Atacadista, após receber uma promoção. Minhas responsabilidades incluíam o fechamento de caixa dos mercados, garantindo a precisão e organização dos registros financeiros.',
+      work: 'Super Rede Atacadista',
+      year: '2023',
+      image: '../../../assets/imagens/promocao.png',
+    },
+    {
+      aboutMe: 'Desde janeiro de 2024, após mudar de cidade, eu trabalho na área da computação na empresa Consilux Consultoria. Durante esse período, aprendi a lidar com C# e SQL Server e, atualmente, atuo como desenvolvedor FrontEnd, programando em Angular.',
+      work: 'Consilux Consultoria',
+      year: '2024',
+      image: '../../../assets/imagens/TI.jpg',
+    },
+    {
+      aboutMe: '',
+      work: '',
+      year: '',
+      image: '../../../assets/imagens/jornadaContinua.png',
+    }
+  ]
 }
--- src/app/components/trail-about-me/trail-about-me.component.ts
+++ src/app/components/trail-about-me/trail-about-me.component.ts
@@ -8,16 +8,9 @@ import { Component, Input } from '@angular/core';
   styleUrl: './trail-about-me.component.scss'
 })
 export class TrailAboutMeComponent {
-  @Input ({ alias: 'aboutMeRight'}) textAboutMeRight: string = '';
-  @Input ({ alias: 'aboutWorkRight'}) textAboutWorkRight: string = '';
-  @Input ({ alias: 'aboutNumberRight'}) numberAboutYearRight: number = 0;
-
-  @Input( { alias: 'aboutMeLeft1' }) textAboutMeLeft1: string = '';
-  @Input( { alias: 'aboutMeLeft2' }) textAboutMeLeft2: string = '';
-
-  @Input ({ alias: 'aboutWorkLeft1'}) textAboutWorkLeft1: string = '';
-  @Input ({ alias: 'aboutWorkLeft2'}) textAboutWorkLeft2: string = '';
-
-  @Input ({ alias: 'aboutNumberLeft1'}) numberAboutYearLeft1: number = 0;
-  @Input ({ alias: 'aboutNumberLeft2'}) numberAboutYearLeft2: number = 0;
+  @Input( { alias: 'aboutMe' }) textAboutMe: string = '';
+  @Input ({ alias: 'aboutWork'}) textAboutWork: string = '';
+  @Input ({ alias: 'aboutYear'}) numberAboutYear: string = '';
+  @Input ({ alias: 'aboutImage'}) aboutImage: string = '';
+  @Input ({ alias: 'isLeft' }) isLeft: boolean = true;
 }
